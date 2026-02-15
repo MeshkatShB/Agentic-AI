3ff137bb0b9215f548f179143b7e9d40dcaca6f5
meshkatshb <meshkat97@yahoo.com>
2026-02-15 11:49:08 +0330

    faet(tools): exchange MS tool

--- backend/tools/implementations/__init__.py
+++ backend/tools/implementations/__init__.py
@@ -4,15 +4,35 @@ from .file_tools import SearchLocalFilesTool, ReadFileTool, ParseDocumentTool
 from .web_tools import WebSearchTool, WebScrapeTool, HttpRequestTool
 from .database_tools import DatabaseQueryTool
 from .api_tools import WeatherAPITool, CustomAPITool
+from .cron_job_tools import ScheduleJobTool
+from .exchange_tools import (
+    EXCHANGE_TOOL_NAMES,
+    ExchangeListEmailsTool,
+    ExchangeGetEmailTool,
+    ExchangeSendEmailTool,
+    ExchangeListCalendarTool,
+    ExchangeCreateEventTool,
+    ExchangeListTasksTool,
+    ExchangeCreateTaskTool,
+)
 
 __all__ = [
     "SearchLocalFilesTool",
-    "ReadFileTool", 
+    "ReadFileTool",
     "ParseDocumentTool",
     "WebSearchTool",
     "WebScrapeTool",
     "HttpRequestTool",
     "DatabaseQueryTool",
     "WeatherAPITool",
-    "CustomAPITool"
+    "CustomAPITool",
+    "ScheduleJobTool",
+    "EXCHANGE_TOOL_NAMES",
+    "ExchangeListEmailsTool",
+    "ExchangeGetEmailTool",
+    "ExchangeSendEmailTool",
+    "ExchangeListCalendarTool",
+    "ExchangeCreateEventTool",
+    "ExchangeListTasksTool",
+    "ExchangeCreateTaskTool",
 ]
